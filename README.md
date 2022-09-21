efee9474a24c49db0c2a74d77d591e244ceef68f
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2022-09-21 10:01:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# Banky Website
+## Basic Bank System For The Spark Foundation GRIP 
+##### Develop By Menna Mohamed
+
+Banky is a simple dynamic website which has the following specs.
+
+- View all customers
+- Transfer money between customers
+- Show transfer history 
+
+## ✨ Technologies
+### Front-End
+
+- HTML 
+- CSS 
+- JavaScript
+- Bootstrap 5 
+- jQuery 
+
+### Back-End
+
+- Express.js 
+- MongoDB
+
+
+
+
+## ✨ Installation
+
+Banky requires [Node.js](https://nodejs.org/) v10+ to run.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+cd Banky-Website
+npm i
+npm run watch
+```
+
+
+
---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+var $ = require("jquery");
+
+
+const customerRoutes = require('./routes/customerRoute')
+// const transferRoutes = require('./routes/transferRoute')
+// const historyRoutes = require('./routes/historyRoute')
+
+// const Customers = require("./models/customersSchema");
+// const History = require("./models/historySchema");
+
+//auto refresh
+
+const path = require("path");
+
+const livereload = require("livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+const connectLivereload = require("connect-livereload");
+const { default: mongoose } = require("mongoose");
+app.use(connectLivereload());
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+//Database connection
+const mongo = require("mongoose");
+mongo
+  .connect(
+    "mongodb+srv://Banky:123@cluster0.wmbhi.mongodb.net/BankDB?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(process.env.PORT  || port, () => {
+      console.log(`Example app listening on port at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.redirect("index");
+});
+
+// app.get("/index", (req, res) => {
+//   res.render("index.ejs");
+// });
+app.use('/index',customerRoutes)
+
+// app.use('/transfer',transferRoutes)
+
+
+
+app.use((req, res) => {
+  res.status(404).send("Sorry page not found !");
+});
